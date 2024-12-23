b571e2d2662c7637674875b0a92953765e36b701
andriyyya <andreapayotjandog@gmail.com>
2024-12-23 15:07:26 +0700

    Update globalVariable.java

--- src/Function/globalVariable.java
+++ src/Function/globalVariable.java
@@ -26,7 +26,7 @@ public class globalVariable {
     public static ArrayList<Staff> sortedStaffListASC = new ArrayList<>();
     public static ArrayList<Staff> sortedStaffListDESC = new ArrayList<>();
     public static ArrayList<Student> sortedStudentListASC = new ArrayList<>();
-    public static ArrayList<Student> sortedStudentListDESC = new ArrayList<>();
+
 
 
 
